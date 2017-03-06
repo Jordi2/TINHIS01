75df660e2c0e21aacf993e48ec6b1765d0c97bb1
JLU_0889529 <jordi.luijk@live.nl>
2017-03-06 13:45:08 +0100

    Added display for people integer

--- getDistance/getDistance.ino
+++ getDistance/getDistance.ino
@@ -34,10 +34,54 @@ int averageOf = 25;                                 //take average of *averageOf
 int averageDistanceA = 0; int averageDistanceB = 0; //average of measurments
 int range = 25;                                     //allowed distance between sensors before detecting people
 
-int people = 0;                                     //the number of people that have passed the sensor
+int people = 1;                                     //the number of people that have passed the sensor
+
+//variables for display en shift register
+int latchPin = 8;   //Pin connected to ST_CP rck of 74HC595 
+int clockPin = 12;  //Pin connected to SH_CP sck of 74HC595
+int dataPin = 11;   //Pin connected to DS ser of 74HC595
+
+byte dataArray[12];
+ 
+int curNum = 1; //digit to display
+
+//digits 
+int dig1 = 4; 
+int dig2 = 3; 
+int dig3 = 2; 
+int dig4 = 1; 
+
+int del = 1;//delay time for multiple unique digits 
 
 void setup()
 {
+  //set pins to output so you can control the shift register
+  pinMode(latchPin, OUTPUT);//RCK
+  pinMode(clockPin, OUTPUT);//SCK
+  pinMode(dataPin, OUTPUT);//SER
+  
+  dataArray[0] = 0; //uit 
+  dataArray[1] = 144; //1=b+c=16+128 
+  dataArray[2] = 91; //2=abged=8+16+64+1+2=91 
+  dataArray[3] = 218; //3=abcdg=8+16+128+2+64=218 
+  dataArray[4] = 240; //4=fgbc=32+16+64+128=240 
+  dataArray[5] = 234; //5 
+  dataArray[6] = 235; //6 
+  dataArray[7] = 152; //7 
+  dataArray[8] = 251; //8 
+  dataArray[9] = 248; //9 
+  dataArray[10] = 187; // 
+  
+  //pins for controlling digit selection 
+  pinMode(dig1, OUTPUT); 
+  digitalWrite(dig1, LOW); 
+  pinMode(dig2, OUTPUT); 
+  digitalWrite(dig2, LOW); 
+  pinMode(dig3, OUTPUT); 
+  digitalWrite(dig3, LOW); 
+  pinMode(dig4, OUTPUT); 
+  digitalWrite(dig4, LOW); 
+  
   Serial.begin(9600); //Enable the serial comunication
 }
 
@@ -71,7 +115,16 @@ void loop()
       Serial.println("-------------Right-------------");
       people++;
     }
+    if((averageDistanceA < averageDistanceB-range) || (averageDistanceA > averageDistanceB+range)){
+      multiDigitUpdate();  
+      delay(500);//this delay is to prevent the people counter from counting one person twice
+    }
+    Serial.println(people);
   }
+  multiDigitUpdate();
+  //updateDisplay();
+
+  if(people == 10){people = 1;}
 }
 
 int IsInSensorTypeRange(int dis){
@@ -86,3 +139,44 @@ int IsInSensorTypeRange(int dis){
   }
   return dis;
 }
+
+void clearLEDs(){ 
+  curNum = 0; 
+  updateDisplay(); 
+} 
+void multiDigitUpdate(){ 
+  /* for other digits if you need them
+  clearLEDs(); 
+  digitalWrite(dig1, LOW); 
+  digitalWrite(dig2, HIGH); 
+  digitalWrite(dig3, HIGH); 
+ // curNum = 0; 
+  updateDisplay(); 
+  delay(del); 
+  
+  clearLEDs(); 
+  digitalWrite(dig1, HIGH); 
+  digitalWrite(dig2, LOW); 
+  digitalWrite(dig3, HIGH); 
+  //curNum = 0; 
+  updateDisplay(); 
+  delay(del);
+  */
+  clearLEDs(); 
+  digitalWrite(dig1, HIGH); 
+  digitalWrite(dig2, HIGH); 
+  digitalWrite(dig3, LOW); 
+  updateDisplay(); 
+  delay(del); 
+} 
+void updateDisplay(){ 
+  // take the latchPin low so 
+  // the LEDs don't change while you're sending in bits: 
+  digitalWrite(latchPin, LOW); 
+  // shift out the bits: 
+  shiftOut(dataPin, clockPin, MSBFIRST, dataArray[people]);
+  //take the latch pin high so the LEDs will light up: 
+  digitalWrite(latchPin, HIGH); 
+  // pause before next value: 
+  //delay(10); 
+} 
